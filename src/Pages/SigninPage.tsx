348f659532c7f64b5f1c47657ab1fbc5897b8d8e
Idyege Nathaniel <idyegenathaniel@gmail.com>
2024-11-06 11:00:54 +0100

    Update

--- src/Pages/SigninPage.tsx
+++ src/Pages/SigninPage.tsx
@@ -1,10 +1,52 @@
-import { Link } from "react-router-dom"
+import axios, { AxiosResponse } from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const SigninPage = () => {
+interface ApiRespopnse {
+  token: string;
+}
+interface SigninPageProps {
+  name: string;
+  password: string;
+}
 
+const instance = axios.create({
+  baseURL: "https://api.cashflow.rehx.name.ng"
+});
 
+const SigninPage: React.FC<SigninPageProps> = () => {
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string | number>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!name || !password) {
+      setError("Both Fields are required");
+      return;
+    }
+    try {
+      const response: AxiosResponse<ApiRespopnse> = await instance.post("/sign-in", {
+        name,
+        password,
+      });
+      const token = response.data.token;
+
+      if (token) {
+        localStorage.setItem('token', token);
+      } else {
+        setError("Invalid Log in response from server")
+      }
+    } catch (error) {
+      setError("Login failed, try again");
+    }
+  };
+
+  //STYLING
   const signInClass = "w-full flex items-center justify-center py-20";
   const signInContainerClass = "bg-[#dadad9] max-w-xl mx-4 md:m-auto rounded-md shadow-md px-4 py-2 text-black";
+  const formClass = "flex flex-col gap-y-2 py-6";
   const googleButton = "bg-blue-500 hover:bg-blue-600 mx-auto py-2 px-4 cursor-pointer text-white font-bold rounded-md shadow-lg";
   const signButton = "bg-green-500 hover:bg-green-600 py-2 px-4 mb-2 mt-2 cursor-pointer text-white font-bold";
   const signInputClass = "w-full py-3 px-2 mb-2 outline-none";
@@ -13,20 +55,43 @@ const SigninPage = () => {
     <section className={signInClass}>
       <div className="container mx-auto">
         <div className={signInContainerClass}>
-  
-          <form action="" className="flex flex-col gap-y-2 py-6">
-          <h2 className="text-2xl text-center font-bold mb-4">Login</h2>
-            <input type="text" name="name" placeholder="Enter Name" className={signInputClass} />
-            <input type="password" name="" placeholder="Enter Password" className={signInputClass} />
-            <button type="submit" className={signButton}>Sign In</button>
+          <form onSubmit={submitForm} className={formClass}>
+            <h2 className="text-2xl text-center font-bold mb-4">Login</h2>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter Name"
+              className={signInputClass}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+              className={signInputClass}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className={signButton}>
+              Sign In
+            </button>
             <p className="text-center text-xs">Or</p>
-            <button type="submit" className={googleButton}>Continue With Google</button>
-            <p className="text-xs text-center">Don't have an account? <Link to={"/sign-up"} className="underline">Sign Up</Link> </p>
+            <button type="submit" className={googleButton}>
+              Continue With Google
+            </button>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <p className="text-xs text-center">
+              Don't have an account?{" "}
+              <Link to={"/sign-up"} className="underline">
+                Sign Up
+              </Link>{" "}
+            </p>
           </form>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SigninPage
+export default SigninPage;
--- src/Pages/SignupPage.tsx
+++ src/Pages/SignupPage.tsx
@@ -1,26 +1,79 @@
-import { Link } from "react-router-dom"
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignupPage = () => {
-  return(
-    <section className="w-full py-20 flex items-center justify-center">
+  const [fullName, setFullName] = useState<string>("");
+  const [mail, setMail] = useState<string | number>("");
+  const [password, setPassword] = useState<string | number>("");
+
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+
+  //STYLING
+  const signUpClass = "w-full py-20 flex items-center justify-center";
+  const containerClass = "bg-[#dadad9] max-w-xl mx-4 md:m-auto rounded-md shadow-md px-4 py-2 text-black";
+  const formClass = "flex flex-col gap-y-2 py-6";
+  const inputClass = "w-full py-3 px-2 mb-2 outline-none";
+  const regButClass = "bg-green-500 hover:bg-green-600 py-2 px-4 mb-2 mt-2 cursor-pointer text-white font-bold";
+  const regGoogleClass = "bg-blue-500 hover:bg-blue-600 py-2 px-4 mx-auto cursor-pointer text-white font-bold rounded-md shadow-lg";
+
+
+  return (
+    <section className={signUpClass}>
       <div className="container mx-auto">
-        <div className="bg-[#dadad9] max-w-xl mx-4 md:m-auto rounded-md shadow-md px-4 py-2 text-black">
-  
-          <form action="" className="flex flex-col gap-y-2 py-6">
-          <h2 className="text-2xl text-center font-bold mb-4">Register</h2>
-            <input type="text" name="name" placeholder="Enter Name" className="w-full py-3 px-2 mb-2 outline-none" />
-            <input type="email" name="email" placeholder="Enter Email" className="w-full py-3 px-2 mb-2 outline-none" />
-            <input type="password" name="" placeholder="Enter Password" className="w-full py-3 px-2 mb-4 outline-none"/>
-            <button type="submit" className="bg-green-500 hover:bg-green-600 py-2 px-4 mb-2 cursor-pointer text-white font-bold">Submit</button>
+        <div className={containerClass}>
+          <form onSubmit={submitForm} className={formClass}>
+            <h2 className="text-2xl text-center font-bold mb-4">Register</h2>
+            <input
+              type="text"
+              name="fullname"
+              placeholder="Enter Fullname"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              className={inputClass}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
+              className={inputClass}
+            />
+            <input
+              type="password"
+              name=""
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className={inputClass}
+            />
+            <button
+              type="submit"
+              className={regButClass}
+            >
+              Submit
+            </button>
             <p className="text-center text-xs">Or</p>
-            <button type="submit" className="bg-blue-500 hover:bg-blue-600 py-2 px-4 mx-auto cursor-pointer text-white font-bold rounded-md shadow-lg">Continue With Google</button>
-            <p className="text-xs text-center">Already have an account? <Link to={"/sign-in"} className="underline">Sign in</Link> </p>
+            <button
+              type="submit"
+              className={regGoogleClass}
+            >
+              Continue With Google
+            </button>
+            <p className="text-xs text-center">
+              Already have an account?{" "}
+              <Link to={"/sign-in"} className="underline">
+                Sign in
+              </Link>{" "}
+            </p>
           </form>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;
